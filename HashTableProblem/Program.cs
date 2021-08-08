cf883fb4fd0822704e3c293c6b3728560c452fdf
r-svp26 <riteshk6341@gmail.com>
2021-08-08 14:03:59 +0530

    [Ritesh] Add: Display welcome message

---
+++ HashTableProblem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HashTableProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To HashTable Programs.");
+        }
+    }
+}
