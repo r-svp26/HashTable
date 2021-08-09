d1166310325f18269b094aa21a25e6f58ef2d1a4
r-svp26 <riteshk6341@gmail.com>
2021-08-09 18:27:37 +0530

    [Ritesh] Add: Frequency of large paragraph using HashTable

--- HashTableProblem/Program.cs
+++ HashTableProblem/Program.cs
@@ -10,17 +10,32 @@ namespace HashTableProblem
         /// </summary>
         public static void HashOperation()
         {
-            hash.Add("0", "to");
-            hash.Add("1", "be");
-            hash.Add("2", "or");
-            hash.Add("3", "not");
-            hash.Add("4", "to");
-            hash.Add("5", "be");
+            hash.Add("0", "Paranoids");
+            hash.Add("1", "are");
+            hash.Add("2", "not");
+            hash.Add("3", "paranoid");
+            hash.Add("4", "because");
+            hash.Add("5", "they");
+            hash.Add("6", "are");
+            hash.Add("7", "paranoid");
+            hash.Add("8", "but");
+            hash.Add("9", "because");
+            hash.Add("10", "they");
+            hash.Add("11", "keep");
+            hash.Add("12", "putting");
+            hash.Add("13", "themselves");
+            hash.Add("14", "deliberately");
+            hash.Add("15", "into ");
+            hash.Add("16", "paranoid");
+            hash.Add("17", "avoidable");
+            hash.Add("18", "situations");
             string hash0 = hash.Get("0");
             Console.WriteLine("Oth index value:" + hash0);
             string hash3 = hash.Get("3");
             Console.WriteLine("3rd index value:" + hash3);
             hash.Remove(hash3);
+            string hash10 = hash.Get("10");
+            Console.WriteLine("10th index value:" + hash10);
         }
         /// <summary>
         /// main method
