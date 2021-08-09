67c15d9af25e208b9672cdcdcfc5b9abf45f6960
r-svp26 <riteshk6341@gmail.com>
2021-08-09 18:14:11 +0530

    [Ritesh] Add: Frequency of words in HashTable

---
+++ HashTableProblem/KeyValue.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HashTableProblem
+{
+    public struct KeyValue<K, V>
+    {
+        public K Key { get; set; }
+        public V Value { get; set; }
+    }
+}
--- HashTableProblem/Program.cs
+++ HashTableProblem/Program.cs
@@ -4,9 +4,31 @@ namespace HashTableProblem
 {
     class Program
     {
+        static readonly MyMapNode<string, string> hash = new MyMapNode<string, string>(5);
+        /// <summary>
+        /// print the HashTable operations.
+        /// </summary>
+        public static void HashOperation()
+        {
+            hash.Add("0", "to");
+            hash.Add("1", "be");
+            hash.Add("2", "or");
+            hash.Add("3", "not");
+            hash.Add("4", "to");
+            hash.Add("5", "be");
+            string hash0 = hash.Get("0");
+            Console.WriteLine("Oth index value:" + hash0);
+            string hash3 = hash.Get("3");
+            Console.WriteLine("3rd index value:" + hash3);
+            hash.Remove(hash3);
+        }
+        /// <summary>
+        /// main method
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome To HashTable Programs.");
+            Program.HashOperation();
         }
     }
 }
